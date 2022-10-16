10067e2480bdc9f87a4aff0aca136c1fe88cc57c
credred <cqiufan@outlook.com>
2022-10-17 00:54:43 +0800

    perf(extension): insertScript method added

---
+++ apps/extension/src/lib/insertScript.ts
@@ -0,0 +1,15 @@
+import { runtime } from "webextension-polyfill";
+
+const insertScript = (path: string) => {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'module');
+  script.setAttribute('src', runtime.getURL(path));
+  document.documentElement.appendChild(script);
+
+  return new Promise<void>((resolve, reject) => {
+    script.addEventListener('load', () => resolve())
+    script.addEventListener('error', () => reject())
+  })
+}
+
+export default insertScript;
