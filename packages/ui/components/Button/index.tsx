ea43ddf5580e20b1d4ad3434ef4261add039e34e
credred <cqiufan@outlook.com>
2022-10-22 01:06:14 +0800

    feat(ui): Button component vertical center in default

---
+++ packages/ui/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import { Button as AntdButton, ButtonProps } from "antd";
+import classNames from "classnames";
+import { forwardRef, Ref } from "react";
+import "./index.less";
+
+interface CompoundedComponent extends React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<HTMLElement>> {
+  Group: typeof AntdButton.Group;
+  __ANT_BUTTON: boolean;
+}
+
+const internalButton = (
+  props: ButtonProps,
+  ref: Ref<HTMLElement> | undefined
+) => {
+  return (
+    <AntdButton
+      ref={ref}
+      {...props}
+      className={classNames(
+        props.className,
+        "ui-button"
+      )}
+    />
+  );
+};
+
+const Button = forwardRef(internalButton) as CompoundedComponent;
+
+Button.__ANT_BUTTON = AntdButton.__ANT_BUTTON;
+Button.Group = AntdButton.Group;
+
+export default Button
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,10 +1,12 @@
 import "./themes/index.less";
+import Button from "./components/Button";
 import Table from "./components/Table";
 import Tabs from "./components/Tabs";
 import Editor from "./components/Editor";
 
 export * from 'antd'
 export {
+  Button,
   Table,
   Tabs,
   Editor,
