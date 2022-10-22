cb12e1dde5e48e9f2fea7473e898eec432ecbff7
credred <cqiufan@outlook.com>
2022-10-22 13:00:04 +0800

    fix(ui): monaco editor trigger onChange event unexpectly after update value

--- packages/ui/components/Editor/index.tsx
+++ packages/ui/components/Editor/index.tsx
@@ -3,17 +3,30 @@ import MonacoEditor, { loader } from "@monaco-editor/react";
 import type * as EditorType from "@monaco-editor/react";
 import { FC, useRef } from "react";
 import { Button } from "antd";
+import { usePrevious } from "ahooks";
 
 loader.config({ monaco });
 
 interface EditorProps extends EditorType.EditorProps {
   /** show toolbar */
   toolbar?: boolean;
+  /** An event is emitted when the content of the current model is changed by user input */
+  onChange?: EditorType.OnChange;
+  /** An event is emitted when the content of the current model is changed */
+  onValueChange?: EditorType.OnChange;
 }
 
 const Editor: FC<EditorProps> = (props) => {
-  const { toolbar = true, ...restProps } = props;
+  const { toolbar = true, onValueChange, ...restProps } = props;
+  const { value } = props
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+  const isUserInputRef = useRef(true)
+
+  const previousValue = usePrevious(value)
+
+  if (value !== previousValue) {
+    isUserInputRef.current = false
+  }
 
   const formatCode = () => {
     void editorRef.current?.getAction("editor.action.formatDocument")?.run();
@@ -22,6 +35,15 @@ const Editor: FC<EditorProps> = (props) => {
   const onEditorMount: EditorType.OnMount = (editor, monaco) => {
     editorRef.current = editor;
     props.onMount?.(editor, monaco);
+  };
+
+  const onChange: EditorType.OnChange = (newValue, ev) => {
+    if (isUserInputRef.current) {
+      props.onChange?.(newValue, ev);
+    } else {
+      isUserInputRef.current = true
+    }
+    onValueChange?.(newValue, ev);
   };
 
   return (
@@ -34,6 +56,7 @@ const Editor: FC<EditorProps> = (props) => {
           }}
           {...restProps}
           onMount={onEditorMount}
+          onChange={onChange}
         />
       </div>
       {toolbar && (
