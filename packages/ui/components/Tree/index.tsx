e10ac9f2d4fd29bde9b2c9d241369c1a87351284
credred <cqiufan@outlook.com>
2023-07-14 10:11:21 +0800

    style(ui): improve tree component code

--- packages/ui/components/Tree/index.tsx
+++ packages/ui/components/Tree/index.tsx
@@ -1,10 +1,12 @@
 import { Tree as AntdTree, TreeProps as AntdTreeProps } from "antd";
+import { DataNode } from "antd/es/tree";
 import classNames from "classnames";
+import React from "react";
 import { usePrefixCls } from "../_utils/usePrefixCls";
 import useElementWithStyle from "./style";
 import type { RenderNode } from "./style";
 
-interface TreeProps extends AntdTreeProps {
+interface TreeProps<T extends DataNode = DataNode> extends AntdTreeProps<T> {
   /**
    * compact layout
    * @default false
@@ -12,12 +14,22 @@ interface TreeProps extends AntdTreeProps {
   compact?: boolean;
 }
 
-const Tree: React.FC<TreeProps> = (props) => {
+export type TreeRef = NonNullable<
+  Parameters<typeof AntdTree>[0]["ref"]
+> extends React.Ref<infer R>
+  ? R
+  : never;
+
+const InternalTree = <T extends DataNode = DataNode>(
+  props: React.PropsWithChildren<TreeProps<T>>,
+  ref: React.ForwardedRef<TreeRef>
+) => {
   const { compact, className, ...restProps } = props;
   const { genCls } = usePrefixCls("tree");
 
   const renderNode: RenderNode = (classes) => (
     <AntdTree
+      ref={ref}
       className={classNames(classes, className, compact && genCls("compact"))}
       {...restProps}
     />
@@ -26,4 +38,15 @@ const Tree: React.FC<TreeProps> = (props) => {
   return useElementWithStyle(props.prefixCls, renderNode);
 };
 
-export default Tree;
+type CompoundedComponent = (<T extends DataNode = DataNode>(
+  props: React.PropsWithChildren<TreeProps<T>> & { ref?: React.Ref<TreeRef> }
+) => React.ReactElement) & {
+  TreeNode: typeof AntdTree.TreeNode;
+  DirectoryTree: typeof AntdTree.DirectoryTree;
+};
+
+const Tree = React.forwardRef(InternalTree) as unknown as CompoundedComponent;
+Tree.DirectoryTree = AntdTree.DirectoryTree;
+Tree.TreeNode = AntdTree.TreeNode;
+
+export default InternalTree;
